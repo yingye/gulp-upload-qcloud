e31067001d24bab3917434c3b7403f0f03a277b3
yingye <yingye7667@163.com>
2017-12-10 14:39:32 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
 # gulp-upload-qcloud
+
+腾讯云上传 gulp 插件
+
+## Install
+
+```
+npm install --save-dev gulp-upload-qcloud
+```
+
+## Usage
+
+```js
+const gulp = require('gulp');
+const uploadQcloud = require('gulp-upload-qcloud');
+
+gulp.task('upload', () => {
+  return gulp.src(['test/**/*.*'])
+    .pipe(uploadQcloud({
+      AppId: 'STRING_VALUE',
+      SecretId: 'STRING_VALUE',
+      SecretKey: 'STRING_VALUE',
+      Bucket: 'STRING_VALUE',
+      Region: 'STRING_VALUE',
+      Perfix: 'demo/sub'
+      OverWrite: false
+    }));
+});
+```
+
+## API
+
+### uploadQcloud([options])
+
+#### options
+
+Type: Object
+
+There are 7 options:
+
+* `AppId`(string): 注册或登录 [腾讯云](https://cloud.tencent.com/login) 获取您的AppId。
+* `SecretId`(string): 到 [腾讯云控制台密钥管理](https://console.cloud.tencent.com/capi) 获取您的项目 SecretId 和 SecretKey。
+* `SecretKey`(string): 同 SecretId。
+* `Bucket`(string): 到 [COS 对象存储控制台](https://console.cloud.tencent.com/cos4) 创建存储桶，得到 Bucket（存储桶名称） 和 Region（地域名称）。
+* `Region`(string): Bucket 所在区域。枚举值请见：[Bucket 地域信息](https://cloud.tencent.com/document/product/436/6224)。
+* `Perfix`(string): 自定义文件前缀，例如本地文件路径 img.png ，设置了 `Perfix: 'demo/sub'`，最终腾讯云路径为 `demo/sub/img.png`。
+* `OverWrite`(string): 是否覆盖同名文件，默认 false。
+
+## TIPS
+
+该插件基于 [腾讯云 COS Nodejs SDK V5](https://github.com/tencentyun/cos-nodejs-sdk-v5) 构建，可参考腾讯云官方文档 [Node.js SDK](https://cloud.tencent.com/document/product/436/8629)。
---
+++ index.js
@@ -0,0 +1,117 @@
+var fs = require('fs');
+var path = require('path');
+var gutil = require('gulp-util');
+var through2 = require('through2');
+var Q = require('q');
+var COS = require('cos-nodejs-sdk-v5');
+
+var log = gutil.log;
+var colors = gutil.colors;
+
+module.exports = function (config) {
+  config = config || {};
+  config.AppId = config.AppId || '';
+  config.SecretId = config.SecretId || '';
+  config.SecretKey = config.SecretKey || '';
+  config.Bucket = config.Bucket || '';
+  config.Region = config.Region || '';
+  config.Prefix = config.Prefix || '';
+  config.OverWrite = config.OverWrite || false;
+  // config.Headers = config.Headers || false;
+
+  var existFiles = 0;
+  var uploadedFiles = 0;
+  var uploadedFail = 0;
+  var tasks = [];
+
+  var cos = new COS({
+    AppId: config.AppId,
+    SecretId: config.SecretId,
+    SecretKey: config.SecretKey
+  });
+
+  return through2.obj(function (file, enc, cb) {
+    if (file.isNull()) {
+      return cb();
+    }
+
+    var filePath = file.path;
+    var fileKey = path.join(config.Prefix, path.relative(file.base, file.path));
+
+    var handler = function () {
+      var defer = Q.defer();
+      upload();
+
+      function check (callback) {
+        var defer = Q.defer();
+        cos.headObject({
+          Bucket: config.Bucket,
+          Region: config.Region,
+          Key: fileKey
+        }, function (err, data) {
+          if (err) {
+            log('check-err', err);
+            callback(false);
+          } else {
+            log('Exist ' + fileKey);
+            callback(200 == data.statusCode);
+          }
+        });
+        return defer.promise;
+      }
+
+      function putFile () {
+        cos.putObject({
+            Bucket: config.Bucket,
+            Region: config.Region,
+            Key: fileKey,
+            ContentLength: fs.statSync(filePath).size,
+            Body: fs.createReadStream(filePath),
+            onProgress (progressData) {
+              // console.log(progressData)
+            },
+          }, function (err, data) {
+            if (err) {
+              uploadedFail++;
+              log('err-putObject', err);
+              defer.reject();
+            } else {
+              uploadedFiles++;
+              log(colors.green('Upload ' + fileKey + ' Success'));
+              defer.resolve();
+            }
+          });
+      }
+
+      function upload () {
+        var uploadFlag = true
+        if (!config.OverWrite) {
+          check(function (status) {
+            if (status) {
+              existFiles++;
+              defer.resolve();
+            } else {
+              putFile();
+            }
+          });
+        } else {
+          putFile();
+        }
+      }
+      return defer.promise;
+    }
+    tasks.push(handler());
+
+    cb();
+  }, function () {
+    Q.allSettled(tasks)
+      .then(function (fulfilled) {
+        log('Upload to qcloud: Total:', colors.green(fulfilled.length),
+          'Skip:', colors.gray(existFiles),
+          'Upload:', colors.green(uploadedFiles),
+          'Failed:', colors.red(uploadedFail));
+      }, function (err) {
+        log('Failed upload files:', err);
+      });
+  });
+}
